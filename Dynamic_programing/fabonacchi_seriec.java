c7d00153150419ebd8ef459d4eda29c756813a9f
Digembersharma <digember.gla_cs22@gla.ac.in>
2025-07-07 12:21:11 +0530

    there is question faboncchi_serices

---
+++ Dynamic_programing/fabonacchi_seriec.java
@@ -0,0 +1,29 @@
+package Dynamic_programing;
+
+public class fabonacchi_seriec {
+    /*
+     * public static void main(String[] args) {
+     * int n = 5;
+     * 
+     * int[] dp = new int[n + 1];
+     * dp[0] = 0;
+     * dp[1] = 1;
+     * for (int i = 2; i <= n; i++) { // Fixed: changed from i <= n + 1 to i <= n
+     * dp[i] = dp[i - 1] + dp[i - 2];
+     * }
+     * 
+     * System.out.println("Fibonacci number at position " + n + " is: " + dp[n]);
+     * 
+     * // Print the entire sequence for better understanding
+     * System.out.print("Fibonacci sequence: ");
+     * for (int i = 0; i <= n; i++) {
+     * System.out.print(dp[i] + " ");
+     * }
+     * System.out.println();
+     */
+
+    public static void main(String[] args) {
+        System.out.println("There is no error");
+    }
+}
+
